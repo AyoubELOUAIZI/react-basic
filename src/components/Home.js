a62fa6b04ffd528b76696929322a7ccd35030804
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-07 23:22:26 +0000

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isloading, setIsloading] = useState(true);
+    const [error, setError] = useState(null);
 
     function handeldelete(id) {
         const newblogs = blogs.filter(blog => blog.id !== id);
@@ -15,23 +16,34 @@ const Home = () => {
     });
 
     //hhhhh his code will make the user waite 1s before starting the function fetch
+    //this is not for the real apps it is just kind of semilating what hapent befor fetching data
     useEffect(() => {
         setTimeout(() => {
-            console.log('this useEffect works only in the begining');
-            fetch("http://localhost:7000/blogs")
+            fetch('http://localhost:7000/blogs')
                 .then(res => {
+                    console.log("---------res--------")
+                    console.log(res)
+                    if (!res.ok) { // error coming back from server
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
-                    //    console.log(data);
-                    setBlogs(data);
                     setIsloading(false);
-                });
+                    setBlogs(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    // auto catches network / connection error
+                    setIsloading(false);
+                    setError(err.message);
+                })
         }, 1000);
-    }, []);
+    }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isloading && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title="All MY Blogs" handeldelete={handeldelete} />}
         </div>
