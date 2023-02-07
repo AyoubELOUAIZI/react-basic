f6a8fa5bc5e64218e824d03b95921686b1d31d79
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-07 19:24:28 +0000

    create reuseble components for bloglist

---
+++ src/components/BlogList.js
@@ -0,0 +1,28 @@
+const BlogList = ({ blogs, title }) => {
+    // Destructuring the props object to get the blogs and title data
+    // The equivalent code is:
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    // Logging the blogs data to the console
+    // console.log(blogs);
+
+    // Returning the JSX for the component
+    return (
+        <div className="blog-list">
+            {/* Displaying the title passed as a prop */}
+            <h2>{title}</h2>
+            {/* Mapping over the blogs data to create a preview for each blog */}
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    {/* Displaying the title and author of each blog */}
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+// Exporting the component for use in other parts of the application
+export default BlogList;
