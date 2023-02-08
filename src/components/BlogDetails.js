76ddf34db5d67f93ddb763f9667d1639e1abee3b
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-08 12:22:36 +0000

    complete detailes

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { error, isloading, data: blog } = useFetch('http://localhost:7000/blogs/'+id);
+
 
     return (
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            {/* <h2>Blog details - {id}</h2> */}
+            {isloading && <div>Loading ...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+
         </div>
     );
 }
